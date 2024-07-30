6c3e09589dfbfaa0e9633071af1b5ee54578d6a7
FelipeCarillancaDev <felipe.carillanca@email.com>
2024-07-30 18:16:29 -0400

    se agrega side menu y se generan rutas

--- src/app/reactive/pages/basic-pages/basic-pages.component.ts
+++ src/app/reactive/pages/basic-pages/basic-pages.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basic-pages',
   templateUrl: './basic-pages.component.html',
-  styleUrl: './basic-pages.component.css'
 })
 export class BasicPagesComponent {
 
--- src/app/reactive/pages/switches-pages/switches-pages.component.ts
+++ src/app/reactive/pages/switches-pages/switches-pages.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-switches-pages',
   templateUrl: './switches-pages.component.html',
-  styleUrl: './switches-pages.component.css'
 })
 export class SwitchesPagesComponent {
 
